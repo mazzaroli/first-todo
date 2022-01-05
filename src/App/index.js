762e9ccca816da56c5fee971b72d83fb4366e55d
carlos <carlitosmazaroli@gmail.com>
2022-01-04 22:52:30 -0300

    actualizacion

--- src/App/index.js
+++ src/App/index.js
@@ -1,101 +1,15 @@
 import React from "react";
 import { AppUi } from "./AppUi";
-
-
-// const defaultTodos = [
-//   {text: 'Cortar cebolla', completed: true},
-//   {text: 'Tomar el curso de intro a React', completed: false},
-//   {text: 'Llorar con la llorona', completed: false},
-// ]
-
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-
-  // Revisar si hay Todos guardados, sino crear un array vacio  
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
-
-  const [item, setItem] = React.useState(parsedItem);
-
-  // Local Storage Persistencia de datos
-  const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
-  };
-
-  return [
-    item,
-    saveItem,
-  ];
-}
+import "./App.css";
+import { TodoProvider } from "../TodoContext";
 
 function App() {
-  const [patito, savePatito] = useLocalStorage('PATITO_V1', 'fernando');
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
-  const [searchValue, setSearchValue] = React.useState('');
-  
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-  let searchedTodos = [];
-
-
-  if(!searchValue.length >= 1) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter(todo => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText)
-    });
-  }
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-
-    if (!!newTodos[todoIndex].completed){
-      newTodos[todoIndex].completed = false;
-    } else {
-      newTodos[todoIndex].completed = true;
-    }
-
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1)
-    saveTodos(newTodos);
-  };
-
-  // console.log('Render (antes del user effect)')
-
-  // React.useEffect(() => {
-  //   console.log('use effect')
-  // }, [totalTodos])
-  
-  // console.log('Render (luego del user effect)')
-
+ 
 
   return [
-    <p>{patito}</p>,
-    <AppUi
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />,
+    <TodoProvider>
+      <AppUi/>
+    </TodoProvider>
   ];
 }
 
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch({searchValue, setSearchValue}) {
-    const onSearchValueChange = (event) => {
-        console.log(event.target.value);
-        setSearchValue(event.target.value);
-    }
+function TodoSearch({ searchValue, setSearchValue }) {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
 
-    return [
-        <input 
-            className="TodoSearch" 
-            placeholder="Buscas algun TODO en especifico?"
-            value={searchValue}
-            onChange={onSearchValueChange}
-        />,
-    ];
-};
-  
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
+  );
+}
+
 export { TodoSearch };
