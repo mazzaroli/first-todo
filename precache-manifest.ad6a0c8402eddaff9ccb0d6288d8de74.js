8da93fb088b01e8947402f9c4c6a679d9582b4f1
carlos <carlitosmazaroli@gmail.com>
2022-02-08 21:01:46 -0300

    Updates

---
+++ precache-manifest.ad6a0c8402eddaff9ccb0d6288d8de74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cba7c7fcf9a6c30526088e314081d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8cea8d1e5870d4e3e74",
+    "url": "/static/css/main.f1ee7211.chunk.css"
+  },
+  {
+    "revision": "83f846b46173a28e159c",
+    "url": "/static/js/2.7842ad93.chunk.js"
+  },
+  {
+    "revision": "a8cea8d1e5870d4e3e74",
+    "url": "/static/js/main.3595afa5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
