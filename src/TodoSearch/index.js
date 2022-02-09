dcc84ff19c224d52f2293997d56e51642ab765e9
mazzaroli <71528723+mazzaroli@users.noreply.github.com>
2022-02-08 21:39:08 -0300

    change Onion per Search TODO

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Buscar TODO"
       value={searchValue}
       onChange={onSearchValueChange}
     />
